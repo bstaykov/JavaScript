1fe511c7d1df10a3560870590b030a7977f8efdb
bstaykov <boycho.staykov@gmail.com>
2015-07-29 10:52:08 +0300

    Initial Upload

---
+++ JsTests/tests.js
@@ -0,0 +1,9 @@
+﻿var inputRange = document.getElementById('range');
+var span = document.getElementById('rangeOutput');
+span.innerText = inputRange.value;
+inputRange.addEventListener('change', display);
+
+function display() {
+    var value = inputRange.value;
+    span.innerText = value;
+}
