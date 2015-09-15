23f2d92e9736ee6437b417b34582148d653c9f90
bstaykov <boycho.staykov@gmail.com>
2015-09-15 15:30:49 +0300

    2015 Tests

---
+++ DomUI/scrolling.js
@@ -0,0 +1,34 @@
+﻿(function ($) {
+    var $curr;
+    $.fn.nextOrFirst = function (element, className) {
+        $curr = element;
+        this.on('click', function () {
+            var $next = $curr.next();
+            if ($next.length === 0) {
+                $curr = $curr.siblings().first();
+            } else {
+                $curr = $next;
+            }
+            $curr.addClass(className)
+                .siblings()
+                .removeClass(className);
+        });
+        return this;
+    };
+
+    $.fn.prevOrLast = function (element, className) {
+        $curr = element;
+        this.on('click', function () {
+            var $prev = $curr.prev();
+            if ($prev.length === 0) {
+                $curr = $curr.siblings().last();
+            } else {
+                $curr = $prev;
+            }
+            $curr.addClass(className)
+                .siblings()
+                .removeClass(className);
+        });
+        return this;
+    };
+}(jQuery));
--- JsTests/tests.js
+++
@@ -1,9 +0,0 @@
-﻿var inputRange = document.getElementById('range');
-var span = document.getElementById('rangeOutput');
-span.innerText = inputRange.value;
-inputRange.addEventListener('change', display);
-
-function display() {
-    var value = inputRange.value;
-    span.innerText = value;
-}
